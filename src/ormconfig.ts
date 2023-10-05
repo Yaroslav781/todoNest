6a3119f744c596b9dc682066de32ccd62d48f2e8
Yaroslav781 <pelexach12@gmail.com>
2023-10-05 16:05:35 +0300

    Added my project

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'todo',
+  password: '123',
+  database: 'todo',
+};
+
+export default config;
